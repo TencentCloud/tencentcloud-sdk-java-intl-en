4e773a19678ec193b286ed1410279a9fe5db6c2e
tencentcloudapi <tencentcloudapi@tencent.com>
2026-08-10 03:06:23 +0800

    release 3.1.5

--- src/main/java/com/tencentcloudapi/hunyuan/v20230901/models/SubmitHunyuan3DPartJobResponse.java
+++ src/main/java/com/tencentcloudapi/hunyuan/v20230901/models/SubmitHunyuan3DPartJobResponse.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 public class SubmitHunyuan3DPartJobResponse extends AbstractModel {
 
     /**
-    * Task id (valid period: 24 hours)
+    * <p>Task id (valid period: 24 hours).</p>
     */
     @SerializedName("JobId")
     @Expose
@@ -38,16 +38,16 @@ public class SubmitHunyuan3DPartJobResponse extends AbstractModel {
     private String RequestId;
 
     /**
-     * Get Task id (valid period: 24 hours) 
-     * @return JobId Task id (valid period: 24 hours)
+     * Get <p>Task id (valid period: 24 hours).</p> 
+     * @return JobId <p>Task id (valid period: 24 hours).</p>
      */
     public String getJobId() {
         return this.JobId;
     }
 
     /**
-     * Set Task id (valid period: 24 hours)
-     * @param JobId Task id (valid period: 24 hours)
+     * Set <p>Task id (valid period: 24 hours).</p>
+     * @param JobId <p>Task id (valid period: 24 hours).</p>
      */
     public void setJobId(String JobId) {
         this.JobId = JobId;
